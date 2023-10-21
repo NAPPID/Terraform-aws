e3e5f3cb1e43795cacd71c9d0537e56c82b11404
munngaras783 <vmsai59@gmail.com>
2023-10-21 21:33:09 +0530

    Adding Routing Table to VPC module

--- Modules/vpc/vpc-file.tf
+++ Modules/vpc/vpc-file.tf
@@ -25,6 +25,7 @@ resource "aws_subnet" "pub-sub" {
   count             = length(var.pub-subs)
   cidr_block        = var.pub-subs[count.index]
   availability_zone = local.zone[count.index]
+  map_public_ip_on_launch = "true"
   vpc_id            = local.vpc-id
   tags              = local.all_tags
 }
@@ -38,4 +39,21 @@ resource "aws_nat_gateway" "my-natgate" {
   count = var.nat-enable ? 1 : 0
   subnet_id = aws_subnet.pub-sub[0].id
 
+}
+
+resource "aws_route_table" "nap-route" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+  tags = {
+    "Name" = "module-route"
+  }
+}
+
+resource "aws_route_table_association" "table-assiciation" {
+  route_table_id = aws_route_table.nap-route.id
+  count = length(var.pub-subs)
+  subnet_id = element(aws_subnet.pub-sub.*.id, count.index)
 }
